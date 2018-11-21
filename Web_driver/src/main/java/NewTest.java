0a451b63b9586e9e2d06f3cf46ae2fdbd268292d
Vlad Kapsky <vkapsky@gmail.com>
2018-11-21 22:26:23 +0300

    Web_driver update

--- Web_driver/src/main/java/NewTest.java
+++ Web_driver/src/main/java/NewTest.java
@@ -8,16 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class NewTest {
 
     @Test
-    public void Test () throws InterruptedException{
+    public void Test ()throws InterruptedException{
         System.setProperty("webdriver.chrome.driver", "C://chromedriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.alitalia.com/en_en/");
-        WebElement searchBtn=driver.findElement(By.className("button"));
+        //sleep(1000);
+        WebElement searchBtn=driver.findElement(By.id("validate_date"));
         WebElement err=driver.findElement(By.id("labelErrorInfoStatoVolo"));
+        //WebElement bookBtn=driver.findElement(By.id("cerca-volo__tab cerca-volo__tab--prenota is-active tabs-title"));
         searchBtn.click();
         searchBtn.click();
-        Assert.assertEquals(err,"Data not entered correctly");
+        Assert.assertEquals(err.getText(),"Data not entered correctly");
 
     }
-
 }
