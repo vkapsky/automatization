574e2018b26adba688b5f8e8fcb13475d0caf706
Vlad Kapsky <vkapsky@gmail.com>
2018-11-21 17:23:27 +0300

    web_driver update

--- Web_driver/src/main/java/NewTest.java
+++ Web_driver/src/main/java/NewTest.java
@@ -5,26 +5,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static java.lang.Thread.sleep;
-
-public class FirstTest {
+public class NewTest {
 
     @Test
     public void Test () throws InterruptedException{
         System.setProperty("webdriver.chrome.driver", "C://chromedriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.pobeda.aero/");
-        sleep(10000);
-        WebElement hotelLabel = driver.findElement(By.cssSelector("label[id='label-thirdParties-Hotels']"));
-        hotelLabel.click();
-        sleep(10000);
-        WebElement searchButton = driver.findElement(By.cssSelector("button[id='searchButton']"));
-        searchButton.click();
-        sleep(10000);
-        WebElement tablePrice = driver.findElement(By.cssSelector(".date.active.enabled div div .price"));
-        WebElement totalPrice = driver.findElement(By.cssSelector(".total .right .amountDue"));
-        Assert.assertEquals(tablePrice.getText(),totalPrice.getText());
-        driver.close();
+        driver.get("https://www.alitalia.com/en_en/");
+        WebElement searchBtn=driver.findElement(By.className("button"));
+        WebElement err=driver.findElement(By.id("labelErrorInfoStatoVolo"));
+        searchBtn.click();
+        searchBtn.click();
+        Assert.assertEquals(err,"Data not entered correctly");
+
     }
 
 }
